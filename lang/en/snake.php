05038b6b8b67486a0df4d017194be6ae02b779cf
Jake Dallimore <jake@moodle.com>
2016-04-21 10:45:49 +0800

    First commit, plugin template only

---
+++ lang/en/snake.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for snake
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_snake
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'snake';
+$string['modulenameplural'] = 'snakes';
+$string['modulename_help'] = 'Use the snake module for... | The snake module allows...';
+$string['snakefieldset'] = 'Custom example fieldset';
+$string['snakename'] = 'snake name';
+$string['snakename_help'] = 'This is the content of the help tooltip associated with the snakename field. Markdown syntax is supported.';
+$string['snake'] = 'snake';
+$string['pluginadministration'] = 'snake administration';
+$string['pluginname'] = 'snake';
